c1669b22b748e76fc507f7a51a280416523a793b
Ian Nordby <ian.c.nordby@gmail.com>
2022-10-28 16:42:53 -0600

    Add functionality to display random dish

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
 var buttonLetsCook = document.querySelector('.lets-cook')
 var form = document.querySelector('form')
 var dishSpace = document.querySelector('.recipe-text')
+var potImage = document.querySelector('.pot')
 
-var desserts = ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake', 'Banana Bread', 'Peach Cobbler', 'Cheesecake', 'Funfetti Cake,', 'Baklava', 'Flan', 'Macarons', 'Macaroons', 'Chocolate Cupcakes', 'Pavlova', 'Pumpkin Pie', 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs']
-var mainDishes = ['Spaghetti and Meatballs', 'Pineapple Chicken', 'Shakshuka', 'Thai Yellow Curry', 'Bibimbap',  'Chicken Parmesean', 'Butternut Squash Soup', 'BBQ Chicken Burgers', 'Ramen', 'Empanadas', 'Chicken Fried Rice', 'Sheet Pan Fajitas', 'Margarita Pizza']
-var sides = ['Miso Glazed Carrots', 'Coleslaw', 'Garden Salad', 'Crispy Potatoes', 'Sweet Potato Tots', 'Coconut Rice', 'Caeser Salad', 'Shrimp Summer Rolls', 'Garlic Butter Mushrooms', 'Hush Puppies']
+var meals = {
+  desserts: ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake', 'Banana Bread', 'Peach Cobbler', 'Cheesecake', 'Funfetti Cake,', 'Baklava', 'Flan', 'Macarons', 'Macaroons', 'Chocolate Cupcakes', 'Pavlova', 'Pumpkin Pie', 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs'],
+  mainDishes: ['Spaghetti and Meatballs', 'Pineapple Chicken', 'Shakshuka', 'Thai Yellow Curry', 'Bibimbap',  'Chicken Parmesean', 'Butternut Squash Soup', 'BBQ Chicken Burgers', 'Ramen', 'Empanadas', 'Chicken Fried Rice', 'Sheet Pan Fajitas', 'Margarita Pizza'],
+  sides: ['Miso Glazed Carrots', 'Coleslaw', 'Garden Salad', 'Crispy Potatoes', 'Sweet Potato Tots', 'Coconut Rice', 'Caeser Salad', 'Shrimp Summer Rolls', 'Garlic Butter Mushrooms', 'Hush Puppies']
+}
+// var desserts = ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake', 'Banana Bread', 'Peach Cobbler', 'Cheesecake', 'Funfetti Cake', 'Baklava', 'Flan', 'Macarons', 'Macaroons', 'Chocolate Cupcakes', 'Pavlova', 'Pumpkin Pie', 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs']
+// var mainDishes = ['Spaghetti and Meatballs', 'Pineapple Chicken', 'Shakshuka', 'Thai Yellow Curry', 'Bibimbap',  'Chicken Parmesean', 'Butternut Squash Soup', 'BBQ Chicken Burgers', 'Ramen', 'Empanadas', 'Chicken Fried Rice', 'Sheet Pan Fajitas', 'Margarita Pizza']
+// var sides = ['Miso Glazed Carrots', 'Coleslaw', 'Garden Salad', 'Crispy Potatoes', 'Sweet Potato Tots', 'Coconut Rice', 'Caeser Salad', 'Shrimp Summer Rolls', 'Garlic Butter Mushrooms', 'Hush Puppies']
 
+var currentDish;
+
+buttonLetsCook.addEventListener('click', function(event) {
+  event.preventDefault()
+  console.log([...form.children[1].children].filter(el => el.children[0].checked)[0].children[0].id)
+  var dish = [...form.children[1].children].filter(el => el.children[0].checked)[0].children[0].id
+  getRandomDish(dish)
+  updateDish(currentDish)
+  showDish()
+})
+
+function updateDish() {
+  dishSpace.children[1].innerText = currentDish
+}
+
+function getRandomDish(dish) {
+    var dish = meals[dish][randomIndex(meals[dish])]
+    console.log(dish)
+    currentDish = dish
+}
+
+function showDish() {
+  dishSpace.classList.remove('hidden')
+  potImage.classList.add('hidden')
+}
+
+function randomIndex(arr) {
+    return Math.floor(Math.random() * arr.length)
+}
+// console.log([...form.children[1].children].forEach(el => console.log(el.children[0].className)))
