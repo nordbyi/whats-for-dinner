5fed28aba5d71e338e6649d8e662eac2688d8f7a
Ian Nordby <ian.c.nordby@gmail.com>
2022-10-29 11:44:46 -0600

    Add query selectors for buttons and views

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var buttonLetsCook = document.querySelector('.lets-cook')
 var buttonFavorites = document.querySelector('#favorite')
 var buttonDelete = document.querySelector('#delete')
+var buttonViewFavorites = document.querySelector('.view-favorites')
 var form = document.querySelector('form')
 var dishSpace = document.querySelector('.recipe-text')
 var potImage = document.querySelector('.pot')
 var favoriteDishes = document.querySelector('.favorites-list')
 var formInputToDelete = document.querySelector('#delete-item-form')
+var mainFormView = document.querySelector('.form')
+var mainDisplayView = document.querySelector('.display')
+var favoritesView = document.querySelector('.favorites')
 
 var meals = {
   desserts: ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake', 'Banana Bread', 'Peach Cobbler', 'Cheesecake', 'Funfetti Cake', 'Baklava', 'Flan', 'Macarons', 'Macaroons', 'Chocolate Cupcakes', 'Pavlova', 'Pumpkin Pie', 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs'],
